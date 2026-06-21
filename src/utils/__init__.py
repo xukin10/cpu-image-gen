cd5ba521cd13f0ccfdb3943a4718d0e71f360962
xukin10 <2228677293@qq.com>
2026-06-21 15:30:50 +0800

    Reapply "feat: 添加批量优化功能"
    This reverts commit b91a4e43b649f222a6f382f5b103f82d0cde9cc0.

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -5,3 +5,4 @@
 from .config import config_manager, safe_load_json, ConfigManager
 from .logger import setup_logger, logger
 from .model_cache import model_cache, ModelCache
+from .batch import batch_processor, BatchProcessor
